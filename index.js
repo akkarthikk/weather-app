2e13d9bbf42a31c7abf3c9b636341cbd0bce85b9
KARTHIK AMBARAGONDA <ambaragondakarthik@gmail.com>
2024-06-21 23:48:36 +0530

    apirouting

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import moment from "moment-timezone";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+env.config()
 
 const datew = new Date();
 const dateString = datew.toString();
 const date = dateString.substring(0, 10);
 console.log(date);
-const url = "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=";
-const key = "67baa2a7c1b6bff9cf8911d9ac60abd4";
+const key = process.env.KEY;
 
 
 
 app.get("/", async (req, res) => {
-    const response = await axios.get(url + key);
+    const city = "kolkata";
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=` + key);
     const result = response.data;
     console.log(result);
     const tempCelsius = Celsius(result.main.temp);
@@ -26,15 +29,29 @@ app.get("/", async (req, res) => {
     const temp_Max = Celsius(result.main.temp_max);
     const feel = Celsius(result.main.feels_like);
     const humidity = result.main.humidity;
+
+    const sunrise = convertToIST(result.sys.sunrise);
+    const sunset = convertToIST(result.sys.sunset);
+    console.log(sunset)
     res.render("index.ejs",
-        { date: date, location: result.name, temperature: tempCelsius, min: temp_Min, max: temp_Max, feel: feel, humidity: humidity });
+        {
+            date: date,
+            location: result.name,
+            temperature: tempCelsius,
+            min: temp_Min,
+            max: temp_Max,
+            feel: feel,
+            humidity: humidity,
+            description: result.description
+        });
 });
 
 const Celsius = (kelvin) => {
     return (kelvin - 273.15).toFixed(0);
 };
-
-
+const convertToIST = (timestamp) => {
+    return moment.unix(timestamp).tz('Asia/Kolkata').format('hh:mm A');
+};
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
 });
